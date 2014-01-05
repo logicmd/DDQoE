cfedbdd6cc6b426c88ddb2446ff0ad384a9c13b6
Kevin Tong <logicmd@gmail.com>
2014-01-05 21:36:46 +0800

    added bz2 format compatibility

--- log_parser.py
+++ log_parser.py
@@ -5,11 +5,11 @@
     parser.py是系统模块的名字
 '''
 
-import re, cPickle
+import re, cPickle, bz2
 from request import Request
 
 class LogParser:
-    def __init__(self, file_name=None):
+    def __init__(self, file_name=None, max_line=-1):
         self.file=file_name
         self.request=[]
         self.IP=[]
@@ -18,7 +18,11 @@ class LogParser:
         self.UA=[]
         self.platform=[]
         self.app=[]
-        self.isfull="full" in self.file
+        self.is_bz="bz" in self.file
+        self.is_full=self.is_bz or "full" in self.file
+        self.is_infinite=(max_line<0)
+        self.max_line=max_line
+
 
 
     def proceed(self):
@@ -47,10 +51,24 @@ class LogParser:
             'youtube': 'YoutubeThirdParty'
             }
 
-        if self.isfull:
+        if self.is_bz:
+            pattern = '.+:' + pattern
+
+        if self.is_full:
             pattern += '\s(\d+.\d+.\d+.\d+)'
-        for line in open(self.file, 'r'):
+
+        if '.bz' in self.file:
+            f = bz2.BZ2File(self.file)
+        else:
+            f = open(self.file, 'r')
+
+        for line in f:
             self.parse(line, pattern, ua_list, app_category)
+
+            if not self.is_infinite:
+                self.max_line -= 1
+                if self.max_line<=0:
+                    break
 
 
         #self.parse(open(self.file, 'r').read())
@@ -78,37 +96,36 @@ class LogParser:
     def parse(self, line, pat, ua_list, app_cat):
 
         matched = self.find(pat, line, None)
+        if matched:
+            self.datetime.append(matched[0][1])
+            self.request.append(matched[0][2])
+            if(self.is_full):
+                self.IP.append(matched[0][7])
+            else:
+                self.IP.append(matched[0][0])
 
-        #self.IP.append(matched[0][0])
-        self.datetime.append(matched[0][1])
-        self.request.append(matched[0][2])
-        if(self.isfull):
-            self.IP.append(matched[0][7])
+            _ua = matched[0][6]
+            self.UA.append(_ua)
 
-        _ua = matched[0][6]
-        self.UA.append(_ua)
+            find = False
+            for ua_str in ua_list:
+                if ua_str in _ua:
+                    self.platform.append(ua_str)
+                    find = True
+                    break
 
-        find = False
-        for ua_str in ua_list:
-            if ua_str in _ua:
-                self.platform.append(ua_str)
-                find = True
-                break
+            for app in app_cat:
+                if app in _ua:
+                    self.app.append(app_cat[app])
 
+                    break
 
-
-
-
-        for app in app_cat:
-            if app in _ua:
-                self.app.append(app_cat[app])
-
-                break
-
-        if not find:
-            print _ua
-        #areq = Request(matched[0][7], matched[0][1], matched[0][4], matched[0][6])
-        #print areq
+            if not find:
+                print _ua
+            print matched[0][7], matched[0][1], matched[0][4], matched[0][6]
+        else:
+            print line
+            raise NameError("parsed error")
 
 
 
@@ -121,7 +138,7 @@ class LogParser:
 
 if __name__ == '__main__':
 
-    p=LogParser('log/prod-freewheel.espn.go.com.full.log')
+    p=LogParser('log/prod-freewheel.espn.go.com.log.bz2', 5)
     p.proceed()
     if not len(p.datetime) == len(p.request) == len(p.UA) == len(p.platform) == len(p.app):
         raise IndexError
