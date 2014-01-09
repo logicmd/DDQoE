ed95244e33bff2e989936cde4a3c834f752fb7db
Kevin Tong <logicmd@gmail.com>
2014-01-09 00:00:21 -0800

    update duplicate detection to be more detailed

--- duplicate/dectect_duplicate.py
+++ duplicate/dectect_duplicate.py
@@ -3,6 +3,20 @@ d = {}
 c = 0
 for line in f:
     c+=1
-    d[line] = d.get(line, 0) + 1
-    if d[line] > 1:
-        print "Duplicate line in #" + str(c) + " detected:\n" + line
+    if len(d.get(line,[])) >= 1 and d[line][0] >= 1:
+        d[line][0] += 1
+        d[line].append(c)
+    else:
+        d[line] = []
+        d[line].append(1)
+        d[line].append(c)
+
+for line, line_num_list in d.iteritems():
+    if line_num_list[0] > 1:
+        list_str = []
+        for i in xrange(len(line_num_list)):
+            if i>=1:
+                list_str.append('#%d' %(line_num_list[i]))
+                if i<len(line_num_list) - 1:
+                    list_str.append(', ')
+        print "Duplicate line(%d times) in %s detected:\n%s" %(line_num_list[0], ''.join(list_str), line)
