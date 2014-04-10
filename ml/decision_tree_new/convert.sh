9a2eea1019df18e4deecea8b1540ee4f2adf03e6
Kevin Tong <logicmd@gmail.com>
2014-04-10 07:56:53 -0700

    add more bins

---
+++ ml/decision_tree_new/convert.sh
@@ -0,0 +1,28 @@
+dot -Tpdf iPad_time.dot -o iPad_time.pdf
+dot -Tpdf iPad_ad.dot -o iPad_ad.pdf
+dot -Tpdf Android_time.dot -o Android_time.pdf
+dot -Tpdf Android_ad.dot -o Android_ad.pdf
+dot -Tpdf iPhone_time.dot -o iPhone_time.pdf
+dot -Tpdf iPhone_ad.dot -o iPhone_ad.pdf
+dot -Tpdf Apple TV_time.dot -o Apple TV_time.pdf
+dot -Tpdf Apple TV_ad.dot -o Apple TV_ad.pdf
+dot -Tpdf iPad_time.dot -o iPad_time.pdf
+dot -Tpdf iPad_ad.dot -o iPad_ad.pdf
+dot -Tpdf Android_time.dot -o Android_time.pdf
+dot -Tpdf Android_ad.dot -o Android_ad.pdf
+dot -Tpdf iPhone_time.dot -o iPhone_time.pdf
+dot -Tpdf iPhone_ad.dot -o iPhone_ad.pdf
+dot -Tpdf Apple TV_time.dot -o Apple TV_time.pdf
+dot -Tpdf Apple TV_ad.dot -o Apple TV_ad.pdf
+dot -Tpdf All_time.dot -o All_time.pdf
+dot -Tpdf All_ad.dot -o All_ad.pdf
+dot -Tpdf All_time.dot -o All_time.pdf
+dot -Tpdf All_ad.dot -o All_ad.pdf
+dot -Tpdf iPad_time.dot -o iPad_time.pdf
+dot -Tpdf iPad_ad.dot -o iPad_ad.pdf
+dot -Tpdf Android_time.dot -o Android_time.pdf
+dot -Tpdf Android_ad.dot -o Android_ad.pdf
+dot -Tpdf iPhone_time.dot -o iPhone_time.pdf
+dot -Tpdf iPhone_ad.dot -o iPhone_ad.pdf
+dot -Tpdf Apple TV_time.dot -o Apple TV_time.pdf
+dot -Tpdf Apple TV_ad.dot -o Apple TV_ad.pdf
